3609b0251f9e01bdfe875549b50e2d2bd28f5de0
dharun <dharunpalanisamy5256@gmail.com>
2025-03-15 10:17:35 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import {client_id, client_secret, redirect_uri} from './context/cerdentails'
+import {Route, Routes} from 'react-router-dom'
+import OnboardingLeetcode from "./Pages/OnboardingLeetcode";
 
-function App() {
+const App = () => {
   return (
-    <GoogleOAuthProvider clientId={client_secret}>
-      <div>
-        <h2>Login with Google</h2>
-        <GoogleLogin
-          onSuccess={(response) => {
-            console.log("Google Login Success:", response.credential);
-
-            // Send the token to the backend
-            fetch("http://localhost:5000/auth/google", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ token: response.credential }),
-            })
-              .then((res) => res.json())
-              .then((data) => console.log("Backend Response:", data))
-              .catch((err) => console.error("Error:", err));
-          }}
-          onError={() => console.log("Login Failed")}
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <Routes>
+      <Route path="/leetcodeOnboarding" element={<OnboardingLeetcode/>}/>
+    </Routes>
   );
 }
 
---
+++ src/Pages/OnboardingLeetcode.js
@@ -0,0 +1,11 @@
+const OnboardingLeetcode = () => {
+    return (
+        <>
+         <h1>Onboarding Leetcode</h1>
+         <label htmlFor="LeetcodeId">Leetcode Username : </label>
+         <input type="text" id="LeetcodeId"/>
+         <button>Next -{">"}</button>
+        </>
+    )
+}
+export default OnboardingLeetcode
